a093bfea5f947804dbbc9b75b5b84d4cfdc57b5c
sarahvidal <sarah.vidal@etu.unilim.fr>
2025-01-15 17:49:57 +0100

    change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,22 +2,29 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-			  'sans':["Jost"],
-			  'passion':["Passion One"]
-			},
-			fontSize : {
-			  '11xl':'13rem',
-			  '10xl':'10rem',
-			},
-			backgroundColor:{
-			  'light-red': '#D54F40',
-			},
-			screens:{
-				'xs': '500px',
-			}
-		  },
+	  extend: {
+		fontFamily: {
+		  'sans': ["Jost"],
+		  'passion': ["Passion One"]
+		},
+		fontSize: {
+		  '11xl': '13rem',
+		  '10xl': '10rem',
+		},
+		backgroundColor: {
+		  'light-red': '#D54F40',
+		},
+		screens: {
+		  'xs': '500px',
+		},
+		translate: {
+		  '33': '8.45rem',
+		  '38': '9.3rem',
+		  '13': '3.1rem',
+		  '18': '4.4rem',
+		}
+	  },
 	},
 	plugins: [],
-}
+  }
+  
